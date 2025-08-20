7cf5978533f2a2113698dbb1379c0467e9a0d9ca
feliperezende2007 <feferezende01@gmail.com>
2025-08-20 10:06:11 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+
+ #include <stdio.h>
+ int main()
+ {
+ int i;
+ float f;
+ char c;
+
+ printf("Digite um caractere: ");
+ scanf();// complete o comando para ler a variável c
+ printf("Digite um valor inteiro: ");
+ scanf(); // complete o comando para ler a variável i
+ printf("Entre com um numero de ponto flutuante (valor nao inteiro): ");
+ scanf();// complete o comando para ler a variável f
+ printf (); // imprimeir as variável
+
+ return 0;
+ }
+
+
+
+
+
